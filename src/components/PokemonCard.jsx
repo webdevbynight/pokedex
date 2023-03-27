6aeab1f7a02700cdc634b218ad166fd4b42b7781
webdevbynight <contact@victor-brito.name>
2023-03-27 19:36:32 +0200

    Made “PokemonCard” component dynamic with some data

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,21 @@
+const pokemonList =
+[
+    {
+        name: 'bulbasaur',
+        imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+    },
+    {
+        name: 'mew'
+    }
+];
+
 function PokemonCard()
 {
+    let pokemon = pokemonList[0];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
