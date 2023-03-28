13726e52ea301723a56557946dad75d12681cf13
webdevbynight <contact@victor-brito.name>
2023-03-28 19:29:45 +0200

    Completed the list of pokemons and added “previous” and “next” buttons and the event handlers to update the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import PokemonCard from './components/PokemonCard';
 
@@ -8,19 +9,46 @@ const pokemonList =
         imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
     },
     {
+        name: 'charmander',
+        imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+    },
+    {
+        name: 'squirtle',
+        imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
+    },
+    {
+        name: 'pikachu',
+        imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'
+    },
+    {
         name: 'mew'
     }
 ];
 
 const App = () =>
 {
-    const chosenPokemon = pokemonList[0],
-        appTemplate =
-        (
-            <div>
-                <PokemonCard pokemon={chosenPokemon} />
-            </div>
-        );
+    // Event handlers
+    const handlePrevClick = e =>
+        {
+            setIndex(-- index);
+        },
+        handleNextClick = e =>
+        {
+            setIndex(++ index);
+        };
+
+    // States
+    let [index, setIndex] = useState(0);
+
+    // Generate the template
+    const appTemplate =
+    (
+        <div>
+            <PokemonCard pokemon={pokemonList[index]} />
+            {index > 0 ? <button onClick={handlePrevClick}>Previous</button> : null}
+            {index < pokemonList.length - 1 ? <button onClick={handleNextClick}>Next</button> : null}
+        </div>
+    );
     return appTemplate;
 };
 
